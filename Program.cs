1b2e0b5ac429fdecec5638276252ca090f124a86
dclicktoday <121585981+dclicktoday@users.noreply.github.com>
2023-03-05 16:12:51 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevopTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is a DevOp test project!");
+            while (true)
+            {
+               
+            }
+        }
+    }
+}
